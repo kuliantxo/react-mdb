84e0ae52a7f50f71333cea742bf301b7dbad597e
Julian Miqueo <kuliantxo@gmail.com>
2016-04-22 08:09:27 -0700

    routing

--- src/Index.js
+++ src/Index.js
@@ -1,52 +1,109 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
+import { Link } from 'react-router';
 import App from './components/App';
 import PoweredBy from './components/Powered-by';
 import About from './components/About';
+import { IndexRoute } from 'react-router'
 
 window.React = React;
 
-ReactDOM.render(
-  (<Router history={hashHistory}>
-    <Route path="/" component={App}>
-      <Route path="/about" component={About} />
-      <Route path="/poweredby" component={PoweredBy} />
-    </Route>
-  </Router>), document.getElementById('content')
-);
-
-var CommentBox = React.createClass({
-  onMagicClick: function(event) {
-  event.preventDefault();
-console.log(event);
-console.log(event.target.getAttribute('data-value'));
-  },
+var Comment = React.createClass({
   render: function() {
     return (
-      <div className="dropdown">
-        <button className="btn btn-default dropdown-toggle" type="button" id="{this.props.id}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-          {this.props.name}
-          <span className="caret"></span>
-        </button>
-        <ul className="dropdown-menu" aria-labelledby="{this.props.id}" onClick={this.onMagicClick}>
-          <li><a href data-value="sf">San Francisco</a></li>
-          <li>South San Francisco</li>
-          <li>San Mateo</li>
-          <li>Mountain View</li>
-          <li>San Jose</li>
-        </ul>
+      <div className="well">
+        <h3>
+          {this.props.author}
+        </h3>
+        <img src={this.props.poster} width="60" />
+        <div>{this.props.children}</div>
       </div>
     );
   }
 });
 
-ReactDOM.render(
-  <CommentBox id="departure" name="Departure" />,
-  document.getElementById('departureDropdown')
-);
+var CommentList = React.createClass({
+  render: function() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.title} poster={"http://image.tmdb.org/t/p/w500"+comment.poster_path} key={comment.id}>
+          {comment.overview}
+        </Comment>
+      );
+    });
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
+});
+
+//const Dashboard = () => (
+//    <div>Welcome to the app!</div>
+//);
+var Dashboard = React.createClass({
+  loadCommentsFromServer: function() {
+    $.ajax({
+//      url: 'https://api.themoviedb.org/3/movie/550?api_key=3a3de10833d0c81631b98029f67061a7',
+      url: 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3a3de10833d0c81631b98029f67061a7',
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data.results});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+//    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+        <CommentList data={this.state.data} />
+      </div>
+    );
+  }
+});
+
+
+const Inbox = React.createClass({
+  render() {
+    return (
+      <div>
+        <h2>Inbox</h2>
+        <Link to="/inbox/messages/22">Message 22</Link>
+        {this.props.children || "Welcome to your Inbox"}
+      </div>
+    )
+  }
+})
+
+const Message = React.createClass({
+  render() {
+    return <h3>Message {this.props.params.id}</h3>
+  }
+})
+
 
 ReactDOM.render(
-  <CommentBox id="arrival" name="Arrival" />,
-  document.getElementById('arrivalDropdown')
+  (<Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Dashboard} />
+      <Route path="inbox" component={Inbox}>
+        <Route path="messages/:id" component={Message} />
+      </Route>
+      <Route path="/about" component={About} />
+      <Route path="/poweredby" component={PoweredBy} />
+    </Route>
+  </Router>), document.getElementById('content')
 );
--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,31 @@ import { version } from '../../package.json';
 
 const App = ({ children }) => (
   <div>
-    <header>
-      <h1>React Starterify {version}</h1>
-      <Link to="/about">About</Link>
-      <Link to="/poweredby">Powered by</Link>
-    </header>
-    <section>
-      {children || 'Welcome to React Starterify'}
+    <nav className="navbar navbar-default">
+      <div className="container">
+        <div className="navbar-header">
+          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+          <a className="navbar-brand" href="#">React MDB {version}</a>
+        </div>
+
+        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul className="nav navbar-nav navbar-right">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/inbox">Inbox</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/poweredby">Powered by</Link></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <section className="container">
+      { children }
     </section>
   </div>
 );
